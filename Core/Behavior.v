2dbab68eb441a824ad1b6891f8c91688465e853b
Lucas Pena <lpena7@illinois.edu>
2018-05-21 11:15:39 -0500

    fixed proof of procContractCallTx_LogoutCall
    All cases proved correctly modulo incompleteness of the spec.

--- Core/Behavior.v
+++ Core/Behavior.v
@@ -32,7 +32,32 @@ Proof.
   unfold procContractCallTx, tx_call, tx_sender in H.
   destruct s. repeat right; inversion H; auto.
   destruct (find (logout_validator_index l) (casper_validators st)) eqn:H'.
-  do 4 right. left; exists s; split; auto; exists v; auto.
+  destruct (casper_current_epoch st == block_number %/ casper_epoch_length) eqn:H1.
+  destruct (logout_epoch l <= casper_current_epoch st) eqn:H2.
+  destruct (sigValid_epoch (validator_addr v) (logout_validator_index l)
+                           (logout_epoch l) (logout_sig l)) eqn:H3.
+  destruct (casper_current_dynasty st + casper_dynasty_logout_delay < validator_end_dynasty v)
+           eqn:H4.
+
+  (* Incomplete case: all true *)
+  do 4 right; left. exists s; split; auto; exists v; auto.
+
+  (* current dynasty + logout_delay < end_dynasty = false *)
+  do 3 right; left. exists s; split; auto. exists v; split; auto. split.
+  apply negbT in H4; rewrite leqNgt; auto. inversion H; auto.
+
+  (* Incomplete case: sigValid_epoch ... = false *)
+  do 4 right; left. exists s; split; auto; exists v; auto.
+
+  (* logout_epoch <= current_epoch = false *)
+  do 2 right; left. exists s; split; auto. exists v; split; auto. split.
+  apply negbT in H2; rewrite ltnNge; auto. inversion H; auto.
+
+  (* current_epoch == block_number / epoch_length = false *)
+  right; left. exists s; split; auto. exists v; split; auto. split; move/eqP in H1; auto.
+  inversion H; auto.
+
+  (* sender = None *)
   left; exists s; inversion H; auto.
 Qed.
 
