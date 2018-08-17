8564cea47c5abf6835845815dee0117d167c7685
Lucas Pena <lpena7@illinois.edu>
2018-08-16 21:51:57 -0500

    more encoding of States of new Casper

--- Core/State.v
+++ Core/State.v
@@ -16,14 +16,35 @@ Unset Printing Implicit Defensive.
 (* TODO: are integers ordType? *)
 (* TODO: different sized integers? *)
 (* TODO: "bytes" type? *)
+
+(* TODO: need to figure out which records need equality types *)
+
 Record AttestationRecord {Hash : ordType} :=
   mkAR {
     slot : ordType;
-    shard_id : ordType;
+    shard_id_ar : ordType;
     oblique_parent_hashes: seq Hash;
     shard_block_hash: Hash;
     attester_bitfield: ordType;
     aggregate_sig: seq ordType
+  }.
+
+Record Block {Hash : ordType} :=
+  mkB {
+    (* Hash of the parent block *)
+    parent_hash: Hash;
+    (* Slot number (for the PoS mechanism) *)
+    slot_number: ordType;
+    (* Randao commitment reveal *)
+    randao_reveal: Hash;
+    (* Attestations *)
+    attestations: seq (@AttestationRecord Hash);
+    (* Reference to PoW chain block *)
+    pow_chain_ref: Hash;
+    (* Hash of the active state *)
+    active_state_root: Hash;
+    (* Hash of the crystallized state *)
+    crystallized_state_root: Hash
   }.
 
 Record ActiveState {Hash : ordType} :=
@@ -32,6 +53,22 @@ Record ActiveState {Hash : ordType} :=
     pending_attestations : seq (@AttestationRecord Hash);
     (* Most recent 2*CYCLE_LENGTH block hashes, older to newer *)  
     recent_block_hashes : seq Hash
+  }.
+
+Record CrosslinkRecord {Hash : ordType} :=
+  mkCR {
+    (* What dynasty the crosslink was submitted in *)
+    dynasty: ordType;
+    (* The block hash *)
+    hash: Hash
+  }.
+
+Record ShardAndCommittee :=
+  mkSAC {
+    (* The shard ID *)
+    shard_id_sac: ordType;
+    (* Validator indices *)
+    committee: seq ordType
   }.
 
 Record ValidatorRecord {Hash : ordType} :=
@@ -50,4 +87,34 @@ Record ValidatorRecord {Hash : ordType} :=
     start_dynasty : ordType;
     (* Dynasty where the validator leaves *)
     end_dynasty : ordType
+  }.
+
+Record CrystallizedState {Hash : ordType} :=
+  mkCS {
+    (* List of validators *)
+    validators: seq (@ValidatorRecord Hash);
+    (* Last CrystallizedState recalculation *)
+    last_state_recalc: ordType;
+    (* What active validators are part of the attester set *)
+    (* at what height; and in what shard. Starts at slot *)
+    (* last_state_recalc - CYCLE_LENGTH *)
+    indices_for_heights: seq (seq ShardAndCommittee);
+    (* The last justified slot *)
+    last_justified_slot: ordType;
+    (* Number of consecutive justified slots ending at this one *)
+    justified_streak: ordType;
+    (* The last finalized slot *)
+    last_finalized_slot: ordType;
+    (* The current dynasty *)
+    current_dynasty: ordType;
+    (* The next shard that crosslinking assignment will start from *)
+    crosslinking_start_shard: ordType;
+    (* Records about the most recent crosslink for each shard *)
+    crosslink_records: seq (@CrosslinkRecord Hash);
+    (* Total balance of deposits *)
+    total_deposits: ordType;
+    (* Used to select the committees for each shard *)
+    dynasty_seed: Hash;
+    (* Last epoch the crosslink seed was reset *)
+    dynasty_seed_last_reset: ordType;
   }.
