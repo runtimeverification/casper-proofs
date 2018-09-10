6e2bb94d63d6fccccac8b2bed2f98ffba3181c81
Lucas Pena <lpena7@illinois.edu>
2018-09-10 13:14:13 -0500

    State: updated definitions, fixed bugs

--- Core/State.v
+++ Core/State.v
@@ -13,30 +13,21 @@ Unset Printing Implicit Defensive.
 (* STATE *)
 (* ----- *)
 
-(* TODO: are integers ordType? *)
+Parameter byte : Type.
+
 (* TODO: different sized integers? *)
-(* TODO: "bytes" type? *)
+(* Default m16? *)
 
 (* TODO: need to figure out which records need equality types *)
 
-(* TODO: default values? *)
-
-(* Axiomatic type seq byte *)
-
-(* nat for integers *)
-
-(* Default m16 blah *)
-
-(* Default values, not populated *)
-
 Record AttestationRecord {Hash : ordType} :=
   mkAR {
-    slot : ordType;
-    shard_id_ar : ordType;
+    slot_ar: nat;
+    shard_id_ar: nat;
     oblique_parent_hashes: seq Hash;
     shard_block_hash_ar: Hash;
-    attester_bitfield: ordType;
-    aggregate_sig: seq ordType
+    attester_bitfield: seq byte;
+    aggregate_sig: seq nat
   }.
 
 Record Block {Hash : ordType} :=
@@ -44,7 +35,7 @@ Record Block {Hash : ordType} :=
     (* Hash of the parent block *)
     parent_hash: Hash;
     (* Slot number (for the PoS mechanism) *)
-    slot_number: ordType;
+    slot_number: nat;
     (* Randao commitment reveal *)
     randao_reveal: Hash;
     (* Attestations *)
@@ -68,17 +59,19 @@ Record ActiveState {Hash : ordType} :=
 Record RecentProposerRecord {Hash : ordType} :=
   mkRPR {
     (* Proposer index *)
-    index: ordType;
+    index: nat;
     (* New RANDAO commitment *)
     randao_commitment_rpr: Hash;
     (* Balance delta *)
-    balance_delta: ordType
+    balance_delta: nat
   }.
 
 Record CrosslinkRecord {Hash : ordType} :=
   mkCR {
     (* What dynasty the crosslink was submitted in *)
-    dynasty: ordType;
+    dynasty: nat;
+    (* slot during which crosslink was added *)
+    slot_cr: nat;
     (* The block hash *)
     hash: Hash
   }.
@@ -86,37 +79,37 @@ Record CrosslinkRecord {Hash : ordType} :=
 Record PartialCrosslinkRecord {Hash : ordType} :=
   mkPCR {
     (* What shard is the crosslink being made for *)
-    shard_id_pcr: ordType;
+    shard_id_pcr: nat;
     (* Hash of the block *)
     shard_block_hash_pcr: Hash;
     (* Which of the eligible voters are voting for it (as a bitfield) *)
-    voter_bitfield: ordType
+    voter_bitfield: seq byte
   }.
 
 Record ShardAndCommittee :=
   mkSAC {
     (* The shard ID *)
-    shard_id_sac: ordType;
+    shard_id_sac: nat;
     (* Validator indices *)
-    committee: seq ordType
+    committee: seq nat
   }.
 
 Record ValidatorRecord {Hash : ordType} :=
   mkVR {
     (* The validator's public key *)
-    pubkey : ordType;
+    pubkey : nat;
     (* What shard the validators balance will be sent to after withdrawal *)
-    withdrawal_shard : ordType;
+    withdrawal_shard : nat;
     (* And what address *)
     withdrawal_address : Sender;
     (* The validators current RANDAO beacon commitment *)
     randao_commitment_vr : Hash;
     (* Current balance *)
-    balance : ordType;
+    balance : nat;
     (* Dynasty where the validator is inducted *)
-    start_dynasty : ordType;
+    start_dynasty : nat;
     (* Dynasty where the validator leaves *)
-    end_dynasty : ordType
+    end_dynasty : nat
   }.
 
 Record CrystallizedState {Hash : ordType} :=
@@ -124,27 +117,27 @@ Record CrystallizedState {Hash : ordType} :=
     (* List of validators *)
     validators: seq (@ValidatorRecord Hash);
     (* Last CrystallizedState recalculation *)
-    last_state_recalc: ordType;
+    last_state_recalc: nat;
     (* What active validators are part of the attester set *)
     (* at what height; and in what shard. Starts at slot *)
     (* last_state_recalc - CYCLE_LENGTH *)
-    indices_for_heights: seq (seq ShardAndCommittee);
+    indices_for_slots: seq (seq ShardAndCommittee);
     (* The last justified slot *)
-    last_justified_slot: ordType;
+    last_justified_slot: nat;
     (* Number of consecutive justified slots ending at this one *)
-    justified_streak: ordType;
+    justified_streak: nat;
     (* The last finalized slot *)
-    last_finalized_slot: ordType;
+    last_finalized_slot: nat;
     (* The current dynasty *)
-    current_dynasty: ordType;
+    current_dynasty: nat;
     (* The next shard that crosslinking assignment will start from *)
-    crosslinking_start_shard: ordType;
+    crosslinking_start_shard: nat;
     (* Records about the most recent crosslink for each shard *)
     crosslink_records: seq (@CrosslinkRecord Hash);
     (* Total balance of deposits *)
-    total_deposits: ordType;
+    total_deposits: nat;
     (* Used to select the committees for each shard *)
     dynasty_seed: Hash;
     (* Last epoch the crosslink seed was reset *)
-    dynasty_seed_last_reset: ordType;
+    dynasty_seed_last_reset: nat;
   }.
