6dfec323040e38d0a343d74011176990bbc3575c
Lucas Pena <lpena7@illinois.edu>
2018-09-28 12:42:57 -0500

    Node: rename dummy of type Hash to dummyHash

--- Core/Node.v
+++ Core/Node.v
@@ -13,7 +13,7 @@ Definition NodeId_ordMixin := fin_ordMixin NodeId.
 Canonical NodeId_ordType := Eval hnf in OrdType NodeId NodeId_ordMixin.
 
 (* Parameter of type Hash *)
-Parameter dummy : Hash.
+Parameter dummyHash : Hash.
 
 Parameter BlockVoteCache : Type.
 
@@ -232,7 +232,7 @@ Definition incrementEpoch (st : CasperData) :=
   let: block_number := st.(casper_block_number) in
   let: inc_epoch := current_epoch.+1 in
   if inc_epoch == block_number then
-    let: new_epoch := mkEpochData dummy [::] Unit Unit false false in
+    let: new_epoch := mkEpochData dummyHash [::] Unit Unit false false in
     let: epochs' := upd inc_epoch new_epoch epochs in
     let: st'0 := {[ st with casper_epochs := epochs' ]} in
     let: st'1 := updateExpectedSourceEpoch st'0 in
